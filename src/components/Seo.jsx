60f5afa68a8702e20a8c7ec81a5f5dd18463d832
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2024-01-04 04:19:22 +0100

    added env for dynamic routes

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -10,12 +10,14 @@ const SEO = ({ pageTitle }) => (
       <meta name="description" content="Valentine Tom - Product Manager" />
       <meta name="robots" content="follow" />
       <meta name="googlebot" content="follow" />
-      <meta property="og:image" content="/assets/img/meta.jpg" />
+      <meta
+        property="og:image"
+        content={`${process.env.PUBLIC_URL}/assets/img/meta.jpg`}
+      />{" "}
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-
       <link rel="icon" href="/favicon.ico" />
     </Head>
   </>
